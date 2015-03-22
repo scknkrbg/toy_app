830efd17f851982d05c01ef6282ca130056a3b66
sosyalog <gokbilimci@gmail.com>
2015-03-22 03:00:28 +0200

    Finishing toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  root 'users#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'application#hello2'
+ # root 'application#hello2'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
